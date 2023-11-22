23826da36b91f5501ca00ea919755071e8ff833e
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-11-22 21:18:11 +0530

    server: add folder to zip function

--- server/app/api/routes/routes.py
+++ server/app/api/routes/routes.py
@@ -1,17 +1,30 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 
-from utils.file_traversal import bfs_traversal
+from utils.traverse_file import bfs_traversal
+
+from utils.folder_to_zip import folder_to_zip
 
 router = APIRouter()
 
 @router.get("/doxify_all")
-
 def generate_all_docs():
     try:
 
         bfs_traversal("./files")
 
         return {"message": "All files doxified."}
+    
+    except Exception as e:
+
+        raise Exception(f"{e}")
+    
+@router.get("/zipFile")
+def zip_file():
+    try:
+            
+        response = folder_to_zip("files", "output")
+
+        return {"message": f"response: {response}"}
     
     except Exception as e:
 
